ec9e8baaec05b92ab1088724d65149d70e3d116a
Danilo Medina <danilomedina666@gmail.com>
2024-11-11 21:38:31 -0300

    Se implementa la verificación que no permite el ingreso de un vehículo que ya está en cochera. Se implementa la actualización de estado para cocheras ocupadas y disponibles y se implementa la validación de cocheras disponibles por planta: es decir, si una cochera es ocupada, no está disponible para un registro nuevo hasta que el vehículo que la ocupe se vaya

--- controllers/parking_controller.py
+++ controllers/parking_controller.py
@@ -38,11 +38,10 @@ def registrarEgreso():
         # Llama a la función para obtener el vehículo por patente
         vehiculo = obtener_vehiculo_por_patente(patente)
 
-        if vehiculo:
+        if vehiculo.hora_egreso is None:
             # Cambia el valor del atributo `hora_egreso` por la variable `hora_egreso`
             vehiculo.hora_egreso = hora_egreso
             db.session.commit()  # Guarda los cambios en la base de datos
-
             # Crear y guardar el ticket en la base de datos
             detalle = f"Patente: {vehiculo.patente}, Cliente: {vehiculo.nombre_cliente}, Ubicación: {vehiculo.ubicacion_cochera}"
             ticket = Ticket(
@@ -51,11 +50,12 @@ def registrarEgreso():
                 monto_total=calcular_costo_estacionamiento(vehiculo.hora_ingreso, vehiculo.hora_egreso),
                 detalles=detalle
             )
-
             try:
                 db.session.add(ticket)
                 db.session.commit()
                 flash("Salida registrada y ticket generado correctamente.", "success")
+                vehiculo.ubicacion_cochera = "SALIO" # Se libera la ubicación de la cochera, así está disponible otra vez
+                db.session.commit()
             except Exception as e:
                 db.session.rollback()
                 flash(f"Error al registrar la salida: {str(e)}", "error")
@@ -63,6 +63,7 @@ def registrarEgreso():
             
             # Renderizar el ticket generado en un HTML
             return render_template('ticket_detalle.html', ticket=ticket)
+
         else:
             flash("Vehículo no encontrado en la base de datos.", "error")
 
@@ -84,19 +85,40 @@ def registrarVehiculo():
         # Flash para mostrar ubicación cochera recibida
         flash(f'Ubicación cochera recibida: {ubicacion_cochera}')  
 
-        try:
-            nuevo_vehiculo = Vehiculo(
-                patente=patente, 
-                nombre_cliente=nombre, 
-                hora_ingreso=hora_ingreso, 
-                ubicacion_cochera=ubicacion_cochera
-            )
-            db.session.add(nuevo_vehiculo)
-            db.session.commit()
-            flash("Vehículo registrado correctamente con cochera asignada.")
-        except Exception as e:
-            db.session.rollback()  # Revertir cambios en caso de error
-            flash(f"Ocurrió un error al registrar el vehículo: {str(e)}", "error")
+        # Buscar si ya existe el vehículo
+        vehiculo = obtener_vehiculo_por_patente(patente)
+
+        if vehiculo:
+            if vehiculo.hora_egreso is None:
+                flash("El vehículo ya está en el estacionamiento. No se guarda ingreso", "error")
+                return redirect(url_for('index'))
+            else:
+                #Actualiza los datos para un nuevo ingreso
+                try:
+                    vehiculo.hora_ingreso = hora_ingreso
+                    vehiculo.hora_egreso = None
+                    vehiculo.nombre_cliente = nombre
+                    vehiculo.ubicacion_cochera = ubicacion_cochera
+                    db.session.commit()
+                    flash("Vehículo re-ingresado y datos actualizados correctamente.")
+                except Exception as e:
+                    db.session.rollback()  # Revertir cambios en caso de error
+                    flash(f"Ocurrió un error al registrar el vehículo: {str(e)}", "error")
+                
+        else:
+            try:
+                nuevo_vehiculo = Vehiculo(
+                    patente=patente, 
+                    nombre_cliente=nombre, 
+                    hora_ingreso=hora_ingreso, 
+                    ubicacion_cochera=ubicacion_cochera
+                )
+                db.session.add(nuevo_vehiculo)
+                db.session.commit()
+                flash("Vehículo registrado correctamente con cochera asignada.")
+            except Exception as e:
+                db.session.rollback()  # Revertir cambios en caso de error
+                flash(f"Ocurrió un error al registrar el vehículo: {str(e)}", "error")
 
         return redirect(url_for('index'))  # Redirige a la página de inicio o donde corresponda
 
@@ -106,6 +128,17 @@ def registrarVehiculo():
         'Primer Piso': [f'P1-{i+1}' for i in range(18)],
         'Segundo Piso': [f'P2-{i+1}' for i in range(18)]
     }
+
+    # obtener las cocheras ocupadas
+    cocheras_ocupadas = Vehiculo.query.filter(
+        Vehiculo.hora_egreso == None
+        ).with_entities(Vehiculo.ubicacion_cochera).all()
+    
+    cocheras_ocupadas = [c[0] for c in cocheras_ocupadas] #lista de cocheras ocupadas
+
+    for planta, cocheras in cocheras_disponibles.items():
+        cocheras_disponibles[planta] = [c for c in cocheras if c not in cocheras_ocupadas]
+
     return render_template('register_vehiculo.html', cocheras_disponibles=cocheras_disponibles)
 
 @app.route('/vehiculos', methods=['GET'])
